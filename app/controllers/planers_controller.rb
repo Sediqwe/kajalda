a4d3363b77c8740f4c9a7264229c6938f5ed6766
Sediqwe <sediqwe@gmail.com>
2020-11-01 16:22:26 +0100

    Minden ok

--- app/controllers/planers_controller.rb
+++ app/controllers/planers_controller.rb
@@ -2,18 +2,30 @@ class PlanersController < ApplicationController
   def index
     @planers = Planer.all
   end
+
   def show
-    
+   
   end
+  
   def update
     @planer = Planer.find(params[:id])
-    #render plain: params[:planer].inspect
-    Planer.update(planer_params)
+    respond_to do |format|
+      if @planer.update(planer_params)
+        format.html { redirect_to Planer, alert: 'A heti menü frissítve!' }
+        
+      else
+        format.html { redirect_to Planer, notice: 'Valami gebasz van!!' }
+        
+      end
+    end
   end
   def create
+  
   end
 private
-def planer_params
-  params.require(:planer).permit( recept_ids:[])
+  def planer_params
+    params.require(:planer).permit( recept_ids:[])
+  end
+
 end
-end
+
